92a657669a45c60e08fcb5d73f0d9f803de1e07a
gyojincompany <luckyred@gmail.com>
2025-08-27 17:56:01 +0900

    컨트롤러 에러 메시지 전송 기능 추가

--- src/main/java/com/gyojincompany/member/controller/MemberController.java
+++ src/main/java/com/gyojincompany/member/controller/MemberController.java
@@ -1,9 +1,51 @@
 package com.gyojincompany.member.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.gyojincompany.member.dto.MemberDto;
+import com.gyojincompany.member.validation.MemberValidator;
 
 @Controller
 public class MemberController {
+	
+	@RequestMapping(value = "/memberJoin")
+	public String memberJoin() {
+		
+		return "memberJoin";
+	}
+	
+	@RequestMapping(value = "/memberJoinOk")
+	public String memberJoinOk(@ModelAttribute("memberDto") MemberDto memberDto, Model model, BindingResult result) {
+		
+		MemberValidator memberValidator = new MemberValidator();
+		memberValidator.validate(memberDto, result);
+		
+		if(result.hasErrors()) { //에러가 있는지 확인(유효성 체크)
+			List<ObjectError> allErrors = result.getAllErrors(); //모든 에러 포함
+			List<String> errorMsg = new ArrayList<String>();
+			
+			for(ObjectError error :allErrors) {
+				 //에러메시지
+				errorMsg.add(error.getDefaultMessage());
+			}
+			
+			model.addAttribute("signupError", "회원 가입에 실패하였습니다.");
+			model.addAttribute("errorMsg", errorMsg);
+			
+			return "memberJoin";
+		}
+		
+		
+		return "memberJoinOk";
+	}
 	
 	
 
---
+++ src/main/webapp/WEB-INF/views/memberJoin.jsp
@@ -0,0 +1,32 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>    
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>회원 가입</title>
+</head>
+<body>
+	<h2>회원 가입</h2>
+	<hr>
+	<form action="memberJoinOk" method="post">
+		아이디 : <input type="text" name="id"><br><br>
+		비밀번호 : <input type="password" name="password"><br><br>
+		비밀번호 확인 : <input type="password" name="confirmPassword"><br><br>
+		이름 : <input type="text" name="name"><br><br>
+		나이 : <input type="text" name="age"><br><br>
+		이메일 : <input type="text" name="email"><br><br>
+		<input type="submit" value="가입신청">		
+	</form>
+	<hr>
+	<c:if test="${not empty signupError}">
+		<span style="color:red;">${signupError}</span><br>
+		<ul>
+		<c:forEach items="${errorMsg }" var="errorM">
+			<li>${errorM}</li>
+		</c:forEach>
+		</ul>
+	</c:if>
+</body>
+</html>
